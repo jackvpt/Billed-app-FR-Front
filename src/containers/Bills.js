be170356238327a7633177ac29b3b4ff396b9bed
jackvpt <92818887+jackvpt@users.noreply.github.com>
2025-02-24 22:02:45 +0100

    cleaning

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -40,7 +40,6 @@ export default class {
       return this.store
         .bills()
         .list()
-        .sort((a, b) => new Date(b.date) - new Date(a.date))
         .then((snapshot) => {
           const bills = snapshot.map((doc) => {
             try {
